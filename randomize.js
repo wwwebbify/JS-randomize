194bdd97c3200b4fc9d42c34a1dd0be9f5dc9b78
wwwebbify <david@wwwebbify.com>
2017-01-09 11:26:24 -0700

    v. 1.0

--- randomize.js
+++ randomize.js
@@ -1,18 +1,23 @@
-//RANDOMIZE returns a random number between 'min' - 'max', but will not return 'not'
-function randomize(min, max, not) {
+//RANDOMIZE returns a random integer between 'min' - 'max', but will not return 'not'.
+function randomize(min, max, not = NaN, identifier = 'none set') {
     var loopBreak = 0;
     var loopStop = 100;
-
+    //throw errors if input is no good
+    console.assert(min<max, 'Please place the greater value in the "max" perameter')
+    console.assert(typeof not === 'number', '3rd Parameter - "not" - must be a number')
+    console.assert(typeof identifier === 'string', '4th Parameter - "identifier" -  must be a string')
     do {
+        //Pick a number
         var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
         loopBreak++;
-        console.assert(randomNumber !== not, 'randomize duplicate number - ' + loopBreak)
     } while (loopBreak < loopStop && randomNumber === not);
-
+        //write error if randomNumber still === not
+        console.assert(randomNumber !== not, 'Randomized number equals "not" - randomize function - identifier = ' + identifier)
     if (randomNumber !== not) {
         return randomNumber;
     } else {
-        console.log('duplicate # ' + loopBreak + ' | min = ' + min + ' | max = ' + max + ' | not =' + not);
+        //Return error if Infinite loop has been prevented
+        console.log('Randomization Loop Ended on attempt # ' + loopBreak + ' | min = ' + min + ' | max = ' + max + ' | not =' + not);
         return min
     }
 }
