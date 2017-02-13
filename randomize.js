305cb1a5d2d5eb33341d55e4c0873c690bd1066f
wwwebbify <david@wwwebbify.com>
2017-02-13 16:18:15 -0700

    Improved error information

--- randomize.js
+++ randomize.js
@@ -1,18 +1,18 @@
 //RANDOMIZE returns a random integer between 'min' - 'max', but will not return 'not'.
-function randomize(min, max, not = NaN, identifier = 'none set') {
+function randomize(min = 0, max = 1, not = NaN, identifier = 'none set') {
     var loopBreak = 0;
     var loopStop = 100;
     //throw errors if input is no good
-    console.assert(min<max, 'Please place the greater value in the "max" perameter')
-    console.assert(typeof not === 'number', '3rd Parameter - "not" - must be a number')
-    console.assert(typeof identifier === 'string', '4th Parameter - "identifier" -  must be a string')
+    console.assert(min <= max, 'Please place the greater value in the "max" perameter | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
+    console.assert(typeof not === 'number', '3rd Parameter - "not" - must be a number | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
+    console.assert(typeof identifier === 'string', '4th Parameter - "identifier" -  must be a string | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
     do {
         //Pick a number
         var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
         loopBreak++;
     } while (loopBreak < loopStop && randomNumber === not);
-        //write error if randomNumber still === not
-        console.assert(randomNumber !== not, 'Randomized number equals "not" - randomize function - identifier = ' + identifier)
+    //write error if randomNumber still === not
+    console.assert(randomNumber !== not, 'Randomized number equals "not" - randomize function | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
     if (randomNumber !== not) {
         return randomNumber;
     } else {
