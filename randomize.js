4a2edb2fd182cd09139a5a490eadf2937d4dd85c
David Sutherland <david@wwwebbify.com>
2017-05-18 22:07:13 -0600

    Added array capabilities to 'not'
    Set up 'not' so that it can accept an array [low, high] range to not choose from.

--- randomize.js
+++ randomize.js
@@ -3,30 +3,33 @@
 * You may use or modify it freely as needed. Just notify me in some way, so I can tract how useful it is (star it, watch it, fork it... whatever works for you!)
 */
 
-//RANDOMIZE returns a random number between 'min' - 'max', but will not return 'not'.
-function randomize(min = 0, max = 1, fixed = 0, not = NaN, identifier = 'none set') {
+//RANDOMIZE returns a random integer between 'min' - 'max', but will not return 'not'.
+function randomize(min = 0, max = 1, not = NaN, identifier = 'none set') {
     var loopBreak = 0;
     var loopStop = 100;
     //throw errors if input is no good
     console.assert(min <= max, 'Please place the greater value in the "max" perameter | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
-    console.assert(typeof not === 'number', '3rd Parameter - "not" - must be a number | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
-    console.assert(typeof identifier === 'string', '4th Parameter - "identifier" -  must be a string | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
-    do {
-        //Pick a number
-        var randomNumber = Math.random() * (max - min) + min;
-        if (randomNumber>max){ 
-            console.log(randomNumber);
-            
-            randomNumber=max;};
-        loopBreak++;
-    } while (loopBreak < loopStop && randomNumber === not);
-    //write error if randomNumber still === not
-    console.assert(randomNumber !== not, 'Randomized number equals "not" - randomize function | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
-    if (randomNumber !== not) {
-        return Number(randomNumber.toFixed(fixed));
+    if (typeof not === "object") {
+        console.assert(typeof not[0] === 'number' || typeof not[1], 'If 3rd Parameter is an array - "not[low, high]" - must both be a number | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
     } else {
-        //Return error if Infinite loop has been prevented
-        console.log('Randomization Loop Ended on attempt # ' + loopBreak + ' | min = ' + min + ' | max = ' + max + ' | not =' + not);
+        console.assert(typeof not === 'number', '3rd Parameter - "not" - must be a number | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
+    }//END IF
+    console.assert(typeof identifier === 'string', '4th Parameter - "identifier" -  must be a string | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
+
+    //Pick a number
+    do {
+        var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        loopBreak++;
+    } while (
+        loopBreak < loopStop &&
+        (typeof not === "object" ? randomNumber >= not[0] && randomNumber <= not[1] : randomNumber === not)
+    )//END DO
+
+    //Return error if Infinite loop has been prevented
+    if (loopBreak === 100) {
+        console.warn('Min returned - Randomization Loop Ended on attempt # ' + loopBreak + ' | min = ' + min + ' | max = ' + max + ' | not =' + not);
         return min
-    }
+    }//END IF
+
+    return randomNumber;
 }
