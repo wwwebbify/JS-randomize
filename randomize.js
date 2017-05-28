4da7ef0478f85bcc70dd4469c888ee8ca0f1c6e2
David Sutherland <david@wwwebbify.com>
2017-05-27 23:52:43 -0600

    Clean up
    Just some minor tweaks and house cleaning

--- randomize.js
+++ randomize.js
@@ -3,14 +3,25 @@
 * You may use or modify it freely as needed. Just notify me in some way, so I can tract how useful it is (star it, watch it, fork it... whatever works for you!)
 */
 
-//RANDOMIZE returns a random integer between 'min' - 'max', but will not return 'not'.
+/*
+
+    @public
+    @function randomize
+    @description - Returns a random integer between 'min' - 'max', but will not return 'not'
+    @param {number} [min] - The minimum number to return
+    @param {number} [max] - The maximum number to return
+    @param {number or [low, high]} [not] - The number or array of numbers [low, high] to NOT return (default - NaN)
+    @param {string} - An optional Identifyer used for error tracking, useful to narrow down a problematic function call (default - 'none set')
+
+*/
 function randomize(min = 0, max = 1, not = NaN, identifier = 'none set') {
     var loopBreak = 0;
     var loopStop = 100;
+
     //throw errors if input is no good
-    console.assert(min <= max, 'Please place the greater value in the "max" perameter | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
-    if (typeof not === "object") {
-        console.assert(typeof not[0] === 'number' || typeof not[1], 'If 3rd Parameter is an array - "not[low, high]" - must both be a number | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
+    console.assert(min <= max, 'Please place the greater value in the "max" perameter | min = ' + min + ' | max = ' + max + ' | not[low] = ' + not[0] + ' not[high] = ' + not[1] + ' | identifier = ' + identifier)
+    if (Array.isArray(not)) {
+        console.assert(typeof not[0] === 'number' || typeof not[1] === 'number', 'If 3rd Parameter is an array - "not[low, high]" - must both be a number | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
     } else {
         console.assert(typeof not === 'number', '3rd Parameter - "not" - must be a number | min = ' + min + ' | max = ' + max + ' | not =' + not + ' | identifier = ' + identifier)
     }//END IF
@@ -22,12 +33,12 @@ function randomize(min = 0, max = 1, not = NaN, identifier = 'none set') {
         loopBreak++;
     } while (
         loopBreak < loopStop &&
-        (typeof not === "object" ? randomNumber >= not[0] && randomNumber <= not[1] : randomNumber === not)
+        (Array.isArray(not) ? randomNumber >= not[0] && randomNumber <= not[1] : randomNumber === not)
     )//END DO
 
     //Return error if Infinite loop has been prevented
-    if (loopBreak === 100) {
-        console.warn('Min returned - Randomization Loop Ended on attempt # ' + loopBreak + ' | min = ' + min + ' | max = ' + max + ' | not =' + not);
+    if (loopBreak === loopStop) {
+        console.warn('Min returned - Randomize used at ' + identifier + ' | attempt # ' + loopBreak + ' | min = ' + min + ' | max = ' + max + ' | not =' + not);
         return min
     }//END IF
 
